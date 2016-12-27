bb31309fc487208c27846300c04efe02d2423372
Basheer Becerra <bbecerr@outlook.com>
2016-12-27 17:38:00 -0600

    Ability to retrieve latests mentions from Twitter

---
+++ src/main/java/com/codingbash/MemeTwitterOperations.java
@@ -0,0 +1,5 @@
+package com.codingbash;
+
+public class MemeTwitterOperations {
+
+}
--- src/main/java/com/codingbash/TwitterLogic.java
+++ src/main/java/com/codingbash/TwitterLogic.java
@@ -10,17 +10,27 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TwitterLogic {
-	
+
 	@Autowired
 	private Twitter twitter;
-	
+
+	private Long lastTweetId = null;
+	private int maxPageSize = 200;
+	private long maxTweetId = Long.MAX_VALUE;
+
 	@Scheduled(fixedRate = 5000)
 	public void postTweets() {
 		System.out.println("Posting Tweets:");
 		if (twitter.isAuthorized()) {
 			System.out.println("-- Authorized");
-			List<Tweet> homeTweets = twitter.timelineOperations().getHomeTimeline();
-
+			List<Tweet> homeTweets = (lastTweetId != null)
+					? twitter.timelineOperations().getMentions(20, lastTweetId, 0)
+					: twitter.timelineOperations().getMentions();
+			if (homeTweets.size() > 0) {
+				lastTweetId = homeTweets.get(0).getId();
+				
+				System.out.println("Assigned LastTweetId" + lastTweetId.toString() + ": " + homeTweets.get(0).getText());
+			}
 			for (Tweet tweet : homeTweets) {
 				System.out.println(tweet.getText());
 			}

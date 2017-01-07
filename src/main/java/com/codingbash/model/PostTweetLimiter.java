64af43c9c3f3c6964d27edcf92e1e80ae9bd405f
CodingBash <bbecerr@outlook.com>
2017-01-07 13:04:26 -0600

    Added logging to PostTweetLimiter

--- src/main/java/com/codingbash/model/PostTweetLimiter.java
+++ src/main/java/com/codingbash/model/PostTweetLimiter.java
@@ -1,9 +1,15 @@
 package com.codingbash.model;
 
 import static com.codingbash.constant.MemeConstants.POST_TWEET_INTERVAL_AMOUNT;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PostTweetLimiter {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(PostTweetLimiter.class);
 
 	private AtomicInteger counter;
 
@@ -13,15 +19,19 @@ public class PostTweetLimiter {
 
 	public synchronized boolean permit() throws IllegalStateException {
 		if (counter.get() <= 0) {
+			LOGGER.info("<> #permit() - NOT permitting: counter.get()={}", counter.get());
 			return false;
 		} else {
+			LOGGER.info("<> #permit() - permitting: pre-counter.get()={}", counter.get());
 			counter.decrementAndGet();
 			return true;
 		}
 	}
 
 	public synchronized void refresh() {
+		LOGGER.info("< #refresh() - refreshing");
 		counter.set(POST_TWEET_INTERVAL_AMOUNT);
+		LOGGER.info("> #refresh(): counter.get={}", counter.get());
 	}
 
 	public synchronized int get() {
